bcd3e25a5a1a6e720078c380f0e43a0813307273
Aleia Knight <aleiaknight@gmail.com>
2019-09-02 18:58:24 -0700

    solved f-string problem

--- madlibs.py
+++ madlibs.py
@@ -1,23 +1,17 @@
-#2nd iteration
-
-
 #GLOBAL VARIABLES
 #a list containing the user inputs for call in the f-string of the story
-madlibs = ['one', 'two', 'three', 'four']
+madlibs = ['one', 'two', 'three']
 
 #the core story which will have gaps for insertion
 #a story originally written by Aesop
-story = """A {0} was driving a wagon along a country lane, when the {1} sank down deep into a rut.
- The {2} driver, stupefied and aghast, stood looking at the wagon, and did nothing but utter loud cries to Hercules to come and help him.
+story = """A %s was driving a wagon along a country lane, when the %s sank down deep into a rut.
+ The %s driver, stupefied and aghast, stood looking at the wagon, and did nothing but utter loud cries to Hercules to come and help him.
  Hercules, it is said, appeared and thus addressed him:
  \'Put your shoulders to the wheels, my man. Goad on your bullocks, and never more pray to me for help,
 until you have done your best to help yourself, or depend upon it you will henceforth pray in vain.\'""" 
 
 #FOR LOOPING THOUGH LIST
-#index = 0
-#for index in madlibs:
- #   print(story.format(madlibs[index]))
-#    index +=1
+print(story % tuple(madlibs))
 
 #FUNCTIONS
 #reads user input
