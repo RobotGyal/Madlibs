5703cd7ee34c2cfff906afc4940ec271b09e6869
Aleia Knight <aleiaknight@gmail.com>
2019-09-02 17:47:25 -0700

    Outlining

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+#GLOBAL VARIABLES
+
+
+
+#FUNCTIONS
+
+
+
+#TEST FUNCTION
+
+
+
+#STORY
+#the core story which will have gaps for insertion
+story = ""
+
+
+#MADLIBS
+#a list containing the user inputs for call in the f-string of the story
+madlibs = []
