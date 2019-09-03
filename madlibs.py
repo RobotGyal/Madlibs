8f7d47cf97b9ea3825fbbb6ecc2c81fc7d720f86
Aleia Knight <aleiaknight@gmail.com>
2019-09-02 18:30:44 -0700

    Commiting before new branch

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,20 @@
 #GLOBAL VARIABLES
+#a list containing the user inputs for call in the f-string of the story
+madlibs = ['one', 'two', 'three', 'four']
 
+#the core story which will have gaps for insertion
+#a story originally written by Aesop
+story = """A {0} was driving a wagon along a country lane, when the {1} sank down deep into a rut.
+ The {2} driver, stupefied and aghast, stood looking at the wagon, and did nothing but utter loud cries to Hercules to come and help him.
+ Hercules, it is said, appeared and thus addressed him:
+ \'Put your shoulders to the wheels, my man. Goad on your bullocks, and never more pray to me for help,
+until you have done your best to help yourself, or depend upon it you will henceforth pray in vain.\'""" 
 
+#FOR LOOPING THOUGH LIST
+#index = 0
+#for index in madlibs:
+ #   print(story.format(madlibs[index]))
+#    index +=1
 
 #FUNCTIONS
 #reads user input
@@ -12,15 +26,6 @@ def user_input(prompt):
 def add(item):
     madlibs.append(item)
 
-
-#STORY
-#the core story which will have gaps for insertion
-story = ""
-
-
-#MADLIBS
-#a list containing the user inputs for call in the f-string of the story
-madlibs = []
 
 
 #TEST FUNCTION
